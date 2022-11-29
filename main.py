d5d38412821dfa042af264585b02e74b2c8af883
bnparham <parhambn.workmail@gmail.com>
2022-11-29 16:10:26 +0330

    add new window to program to get the disks number from user input

--- main.py
+++ main.py
@@ -10,42 +10,63 @@ from dllinkedList import Node, doubleLinkedList
 
 
 
-num_of_disk = 4
-
-class mainWindowUI(QDialog):
-    disks_lbl = {}
-    check_col = {
-        'A' : 0,
-        'B' : 0,
-        'C' : 0,
-    }
-    res_forward = []
-    res_backward = []
-    movement = 0
+class firstWindowUI(QDialog):
     def __init__(self):
         super().__init__()
+        loadUi("enternum.ui", self)
+        self.setWindowTitle("Tower of Hanoi GUI")
+        self.setFixedSize(self.width(),self.height())
+        self.runclick.clicked.connect(self.goToMainWindow)
+        self.show()
+        
+    def goToMainWindow(self):
+        self.close()
+        input_txt = int(self.enternumber.text())
+        mainWindow = mainWindowUI(num_of_disk=input_txt)
+        mainWindow.show()
+        
+
+class mainWindowUI(QDialog):
+    def __init__(self,num_of_disk):
+        self.disks_lbl = {}
+        self.check_col = {
+            'A' : 0,
+            'B' : 0,
+            'C' : 0,
+        }
+        self.movement = 0
+        super().__init__()
         loadUi("main.ui", self)
+        self.num_of_disk = num_of_disk
         self.setWindowTitle("Tower of Hanoi GUI")
         self.setFixedSize(self.width(),self.height())
         self.create_disks(self)
         self.dllinked = doubleLinkedList()
-        self.TowerOfHanoi(num_of_disk,'A','C','B')
+        self.TowerOfHanoi(self.num_of_disk,'A','C','B')
         self.cur_next = self.dllinked.head
         self.cur_back = self.dllinked.head
         self.pushButton.clicked.connect(lambda x : self.goToNextRound(self.cur_next))
         self.pushButton_2.clicked.connect(lambda x : self.goToPreviousRound(self.cur_back))
         self.colorBtn.clicked.connect(self.goToChangeColor)
+        self.exitbtn.clicked.connect(self.goToFirstPage)
         self.pushButton_2.setHidden(True)
-        self.check_col["A"] = num_of_disk
-        self.show()       
+        self.check_col["A"] = self.num_of_disk
+        self.show()   
+
+    def goToFirstPage(self):
+        self.close()
+        self.firstWindow = firstWindowUI()
+        self.firstWindow.show()  
+        
+
     def create_disks(self,instance):
         color_range = range(256)
-        biggest = num_of_disk
+        biggest = self.num_of_disk
         count = 0
         biggest_y = 480
         lowest_x = 100
         width = 240
-        for i in range(num_of_disk):
+        for i in range(self.num_of_disk):
             self.disks_lbl[biggest] = QLabel(instance)
             self.disks_lbl[biggest].setStyleSheet("background-color:rgb({},{},{};)".format(choice(color_range),choice(color_range),choice(color_range)))
             self.disks_lbl[biggest].setText(f"{biggest}")
@@ -60,7 +81,7 @@ class mainWindowUI(QDialog):
             
     def goToChangeColor(self):
         color_range = range(256)
-        for i in range(1,num_of_disk+1):
+        for i in range(1,self.num_of_disk+1):
             self.disks_lbl[i].setStyleSheet("background-color:rgb({},{},{};)".format(choice(color_range),choice(color_range),choice(color_range))) 
     
     def goToPreviousRound(self,Node:Node):
@@ -136,12 +157,12 @@ class mainWindowUI(QDialog):
         self.cur_next = self.cur_next.Next
         self.movement += 1
         self.pushButton_2.setHidden(False)
-        if(self.movement == (2**num_of_disk)-1):
+        if(self.movement == (2**self.num_of_disk)-1):
             self.pushButton.setHidden(True)
 
 
 
-    def TowerOfHanoi(self,n=num_of_disk , source="A", destination="C", auxiliary="B"):
+    def TowerOfHanoi(self,n , source, destination, auxiliary):
         if n==1:
             li = [1,source,destination]
             self.dllinked.append(li)
@@ -158,6 +179,7 @@ class mainWindowUI(QDialog):
 
         
 app = QApplication(sys.argv)
-mainUI = mainWindowUI()
+# mainUI = mainWindowUI()
+firstUI = firstWindowUI()
 sys.exit(app.exec_())
 
